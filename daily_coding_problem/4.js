c3bf83e94c25d9e419c74f07cc49c6e0a9dffb5e
miziak <konopie.03-pod@icloud.com>
2024-05-31 18:24:42 +0200

    Added descriptions in files daily_coding_problem/1.js, 2.js, 3.js

--- daily_coding_problem/4.js
+++ daily_coding_problem/4.js
@@ -1,3 +1,7 @@
+//Given an array of integers, find the first missing positive integer in linear time and constant space.
+//In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+//For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
 function problem4 (array) {
     let smallestPositiveNumber = Infinity
     let aLittleLess = null
@@ -17,4 +21,4 @@ function problem4 (array) {
     if (aLittleLess < aLittleMore && aLittleLess > 0) return aLittleLess
     return aLittleMore
 }
-console.log(problem4([1, 6, 7, 8, 9, 10]))
+console.log(problem4([1, 2, 0]))
