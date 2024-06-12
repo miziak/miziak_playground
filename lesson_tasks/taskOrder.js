a0da87a2a641eb464c31fee4d6eaed67674cb531
miziak <konopie.03-pod@icloud.com>
2024-06-12 15:46:56 +0200

    lesson_tasks/taskOrder.js resolved

--- lesson_tasks/taskOrder.js
+++ lesson_tasks/taskOrder.js
@@ -2,11 +2,11 @@
 // Zadanie rozwiąż na sucho bez opdalania go.
 const https = require('https')
 
-setImmediate(()=> console.log('Pumba'))
+setImmediate(()=> console.log('Pumba')) // Macro task 1
 
-console.log(() => console.log('Timon'))
+console.log(() => console.log('Timon')) // 1. console.log
 	
-new Promise((resolve) => {
+new Promise((resolve) => { 
 	console.log('Simba')
 	https.get('https://facebook.com', (response) => {
 		console.log('Rafiki')
@@ -20,15 +20,33 @@ new Promise((resolve) => {
 
 	console.log('Nala')
 
-}).then((result) => console.log(result))
+}).then((result) => console.log(result)) // Micro task 1
 
-console.log('Bunga')
+console.log('Bunga') // 2. console.log
 
-new Promise((resolve) => setTimeout(() => resolve('Sarabi'), 0)).then((resolve) => console.log(resolve))
+new Promise((resolve) => setTimeout(() => resolve('Sarabi'), 0)).then((resolve) => console.log(resolve)) // Micro task 2 w którym znajduje się Macro task setTimeout
 
-console.log('Kiara')
+console.log('Kiara') // 3. console.log
 
 new Promise((resolve) => resolve('Kovu'))
-	.then((result) => console.log(result))
+	.then((result) => console.log(result)) // Micro task 3
 
-console.log('Zira')
+console.log('Zira') // 4. console.log
+
+//Kolejność wyświetlania 
+// 1. console.log(() => console.log('Timon')) 	linijka 7
+// 2. console.log('Bunga') 						linijka 25
+// 3. console.log('Kiara') 						linijka 29
+// 4. console.log('Zira') 						linijka 34
+// Następnie wykonane zostaną Micro taski --------------------
+// Wykonanie Micro taska z 						linijki 9
+// 5. console.log('Simba')
+// 6. console.log('Rafiki')
+// 7. console.log('Mufasa')
+// 8. console.log('Nala')
+// Wykonanie Micro taska z 						linijki 31
+// 9. console.log('Kovu')
+// Wykonanie Micro taska z 						linijki 27
+// 10. console.log('Sarabi')
+// Wykonanie Macro taska z 						linijki 5
+// 11. console.log('Pumba')
